4127639348ba2ce9b283bfef23fb2f80abc32261
Daniel Aristizabal <daniel.aristizabal@sofka.com.co>
2022-11-15 23:08:16 -0500

    Se realiza implemntacion para buscar factura por id
    Se realizacion implementacion para eliminar factura por id

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Body, Param, Post, Delete, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { FacturaDto } from './dto/factura.dto';
 import { FacturaEntity } from './entities/factura.entity';
@@ -11,5 +11,15 @@ export class AppController {
   create(@Body() factura: FacturaDto): Promise<FacturaEntity>{
     const newFactura = new FacturaEntity(factura)
     return this.appService.create(newFactura);
+  }
+
+  @Delete('factura/:id')
+  delete(@Param("id") id: number): Promise<boolean>  {
+    return this.appService.delete(id);
+  }
+
+  @Get("factura/:id")
+  getFacturaById(@Param("id") id: number): Promise<FacturaEntity> {
+    return this.appService.getFacturaById(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,7 @@ import { FacturaEntity } from './entities/factura.entity';
 export class AppService {
   constructor(private datasoruce: DataSource ){}
 
-  async create(factura: FacturaEntity){
+  async create(factura: FacturaEntity): Promise<FacturaEntity> {
     const queryRunner = this.datasoruce.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
@@ -22,5 +22,47 @@ export class AppService {
         HttpStatus.CONFLICT,
       );
     }
+  }
+
+  async delete(id: number): Promise<boolean>  {
+    const queryRunner = this.datasoruce.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    const factura = await this.getFacturaById(id);
+
+    try {
+      await queryRunner.manager.remove(factura.detalleFactura);
+      await queryRunner.manager.remove(factura);
+      await queryRunner.commitTransaction();
+      return Promise.resolve(true);
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw new HttpException(
+        'No se puede eliminar esta factura' + error,
+        HttpStatus.CONFLICT,
+      );
+    }
+  }
+
+  async getFacturaById(id: number): Promise<FacturaEntity> {
+    const queryRunner = this.datasoruce.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    const factura = await queryRunner.manager.findOne(FacturaEntity, {
+      where: {id},
+      relations: {detalleFactura: true},
+    });
+
+    if (!factura) {
+      throw new HttpException(
+        'No existe esta factura con id ' + id,
+        HttpStatus.CONFLICT,
+      );
+    }
+
+    return Promise.resolve(factura);
+      
   }
 }
